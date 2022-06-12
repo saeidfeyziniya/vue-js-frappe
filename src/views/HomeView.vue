2159ac11c25af5276a84285f572e1084472a2368
Saeid Feyziniya <saeid.feyziniya1961@gmail.com>
2022-06-12 14:31:26 +0430

    push details

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>{{ fname }}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      fname: 'saeid'
+    }
+  }
+}
+</script>
